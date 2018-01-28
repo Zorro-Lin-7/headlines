4f8211506333a913dd015463a4717ac250f9f804
linzhun <linzhun7@gmail.com>
2018-01-28 10:55:11 +0800

    Adding looping logic to our template to make the whole list of articles available

--- headlines.py
+++ headlines.py
@@ -16,8 +16,8 @@ BBC_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
 def get_news(publication='bbc'):
     feed = feedparser.parse(BBC_FEED[publication]) # return a dict
     entries = feed['entries']  # return a list
-    first_article = entries[0] # return a dict
-    return render_template('home.html',article=first_article)
+    #first_article = entries[0] # return a dict
+    return render_template('home.html',articles=entries)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
