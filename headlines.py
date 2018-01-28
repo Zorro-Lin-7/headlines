8ee3ac7d2f70148ba508a630c3c25947775bc18a
linzhun <linzhun7@gmail.com>
2018-01-28 15:21:08 +0800

    Getting user input using HTTP GET

--- headlines.py
+++ headlines.py
@@ -1,20 +1,25 @@
 from flask import Flask
 from flask import render_template
 import feedparser
+from flask import request    # for Flask's request context
 
 
 app = Flask(__name__)
 
-BBC_FEED = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
+RSS_FEEDS = {'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
             'cnn': 'http://rss.cnn.com/rss/edition.rss',
             'fox': 'http://feeds.foxnews.com/foxnews/latest',
             'iol': 'http://www.iol.co.za/cmlink/1.640'}
 
 
 @app.route("/")
-@app.route("/<publication>")
-def get_news(publication='bbc'):
-    feed = feedparser.parse(BBC_FEED[publication]) # return a dict
+def get_news():
+    query = request.args.get('publication')
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = 'bbc'
+    else:
+        publication = query.lower()
+    feed = feedparser.parse(RSS_FEEDS[publication]) # return a dict
     entries = feed['entries']  # return a list
     #first_article = entries[0] # return a dict
     return render_template('home.html',articles=entries)
