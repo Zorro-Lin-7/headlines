a9e38683043ad0f07deaeef44bd7dbd3cc5ca880
linzhun <linzhun7@gmail.com>
2018-02-01 18:42:33 +0800

    Adding weather data

--- headlines.py
+++ headlines.py
@@ -1,8 +1,10 @@
+import feedparser
 from flask import Flask
 from flask import render_template
-import feedparser
 from flask import request    # for Flask's request context
-
+import json
+import urllib
+import urllib2
 
 app = Flask(__name__)
 
@@ -21,8 +23,25 @@ def get_news():
         publication = query.lower()
     feed = feedparser.parse(RSS_FEEDS[publication]) # return a dict
     entries = feed['entries']  # return a list
+    weather = get_weather("London,UK")
     #first_article = entries[0] # return a dict
-    return render_template('home.html',articles=entries)
+    return render_template('home.html',
+                            articles=entries,
+                            weather=weather)
 
+
+def get_weather(query):
+    api_url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid='# 加API Key
+    query = urllib.quote(query)
+    url = api_url.format(query)
+    data = urllib2.urlopen(url).read()
+    parsed = json.loads(data)
+    weather = None
+    if parsed.get("weather"):
+        weather = {"description": parsed['weather'][0]['description'],  # 生成一个字典并返回
+                    "temperature": parsed['main']['temp'],
+                    'city': parsed['name']}
+    return weather
+    
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
